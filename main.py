e2f042bffb40016f76cf3848bd4a80f80ec81095
Markus Schmall <schmall.markus@gmail.com>
2016-02-14 15:21:55 +0100

    added support for the head plugin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, request
+from bottle import route, run, template, request, error
 import requests
 import os.path
 import configparser
@@ -23,7 +23,9 @@ def getConfig():
     else:
         return (None, None, None, None)
 
-
+#
+# log data for DTAG TPot
+#
 def logData(attackerIP, attackerRequest, host):
 
     if os.path.isfile('/data/ews/conf/ews.cfg'):
@@ -50,22 +52,8 @@ def postdata(url, content, ip):
 
     nodeid = "elasticpot-" + nodeid
 
-    xml = """<EWS-SimpleMessage version="2.0">
-    <Authentication>
-        <username>tbd</username>
-        <token>tbd</token>
-    </Authentication>
-
-    <Alert>
-        <Analyzer id="_NODEID_"/>
-        <CreateTime tz="+0200">2015-09-09 16:39:21</CreateTime>
-        <Source category="ipv4" port="" protocol="tcp">_IP_</Source>
-        <Target category="ipv4" port="80" protocol="tcp">1.2.3.4</Target>
-        <Request type="url">_URL_</Request>
-        <Request type="raw">_RAW_</Request>
-        <Request type="description">ElasticSearch Honeypot : Elasticpot</Request>
-    </Alert>
-    </EWS-SimpleMessage>"""
+    txt = open("./templates/ews.txt")
+    xml = txt.read()
 
     b = bytes(url, 'utf-8')
     out = base64.b64encode(b)
@@ -75,7 +63,9 @@ def postdata(url, content, ip):
     xml = xml.replace("_RAW_", out.decode("utf-8") )
     xml = xml.replace("_NODEID_", nodeid)
 
+    curDate = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
+    xml = xml.replace("_TIME_", curDate)
 
     headers = {'Content-Type': 'application/xml'}
     requests.post(server, data=xml, headers=headers)
@@ -86,8 +76,12 @@ def pluginhead():
     indexData = txt.read()
     return indexData
 
+#@route('/<index:path>?pretty', method='PUT')
+#def createindex(index):
 
-@route('/')
+
+
+@route('/', method='GET')
 def index():
 
     txt = open("./templates/index.txt")
@@ -95,13 +89,32 @@ def index():
 
     return indexData
 
+#
+# handle not relevant sides
+#
+@error(404)
+def error404(error):
+    txt = open("./templates/404.txt")
+    indexData = txt.read()
+
+    return indexData
+
+
+@route('/_cat/indices', method='GET')
+def getindeces():
+    txt = open("./templates/getindeces.txt")
+    indexData = txt.read()
+
+    return indexData
 
 @route('/_search', method='GET')
 def handleSearchExploitGet():
 
     ip = request.environ.get('REMOTE_ADDR')
+
     postdata(request.url, request.url , ip)
-    return "Found attack: " + request.url
+    print ("Found attack: " + request.url)
+    return ""
 
 @route('/_search', method='POST')
 def handleSearchExploit():
@@ -113,7 +126,12 @@ def handleSearchExploit():
         postContent += l.decode("utf-8")
 
     postdata(request.url, request.url + "Body: " + postContent, ip)
-    return "Found attack: " + request.url + postContent
+
+    print ("Found attack: " + request.url + postContent)
+    return ""
 
 
-run(host='0.0.0.0', port=9200) # host='localhost',
+#
+# listen to all ports to keep
+#
+run(host='0.0.0.0', port=9200)
